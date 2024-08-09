0964c33a7b06acf41c1b3b4c0598bca65aff430d
raffalopez21 <raffalopez@gmail.com>
2024-08-09 01:33:13 -0300

    correccion

--- src/components/Reproductor/Cancion.jsx
+++ src/components/Reproductor/Cancion.jsx
@@ -1,16 +1,47 @@
-import React from 'react';
+import React from "react";
 import {FaEdit, FaTrash} from 'react-icons/fa';
 import './Cancion.css';
+import { useNavigate } from 'react-router-dom';
 
+export default function Cancion({ song, onDelete }) {
+  const navigate = useNavigate();
+    
+    const handleDeleteSongFromAPI = async (id) => {
+        if (window.confirm("¿Estás seguro de que deseas eliminar esta canción?")) {
+            const URL = `https://sandbox.academiadevelopers.com/harmonyhub/songs/${id}/`;
+            const token = localStorage.getItem('authToken');
+            try {
+                const response = await fetch(URL, {
+                    method: 'DELETE',
+                    headers: {
+                        Authorization: `Token ${token}`,
+                    }
+                });
+                console.log('Respuesta de la API:', response);
+                if (response.ok || response.status === 404) {
+                    alert('Canción eliminada exitosamente');
+                    if (onDelete) onDelete(id);
+                } else {
+                    throw new Error('Error al eliminar la canción');
+                }
+            } catch (error) {
+                console.error('Error al eliminar la canción:', error.message);
+                alert('Error al eliminar la canción: ' + error.message);
+            }
+        }
+    };
 
-export default function Cancion({ song }) {
+    const handleEditSong = (id) => {
+      navigate(`/canciones/editar/${id}`);
+  };
+
     return (
         <div className="track">
             <div className="track-id">{song.id}</div>
             <div className="track-info">
                 <div className="track-details">
                     <h3>{song.title}</h3>
-                    <h4>{song.artistName || 'Desconocido'} - {song.albumTitle || 'Desconocido'}</h4>
+                    <h4>{song.artist} - {song.album}</h4>
                     <div className="content">
                         <audio controls>
                             <source src={song.song_file} type="audio/mpeg" />
@@ -21,10 +52,10 @@ export default function Cancion({ song }) {
                 </div>
             </div>
             <div className="track-actions">
-                <button className="edit-button">
+                <button className="edit-button" onClick={() => handleEditSong(song.id)}>
                     <FaEdit /> Modificar
                 </button>
-                <button className="delete-button">
+                <button className="delete-button" onClick={() => handleDeleteSongFromAPI(song.id)}>
                     <FaTrash /> Eliminar
                 </button>
             </div>
